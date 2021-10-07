3f7a9159330c8826ba4f8f93907c05dad25ffc3b
Ted Erickson <ted.erickson@comcast.net>
2021-10-06 19:03:52 -0600

    specify the type of generation strategy

--- src/main/java/com/lpl/errorhandling/model/CityEntity.java
+++ src/main/java/com/lpl/errorhandling/model/CityEntity.java
@@ -5,13 +5,14 @@ import lombok.Data;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity(name = "CITY")
 @Data
 public class CityEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
