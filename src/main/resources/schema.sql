d4896efe48803b646b72b87d431f5ebfae4f6aa7
Ted Erickson <ted.erickson@comcast.net>
2021-10-06 18:45:14 -0600

    fixed schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE CITY (
     id         INTEGER     NOT NULL AUTO_INCREMENT,
-    last_name  VARCHAR(60) NOT NULL,
+    name  VARCHAR(60) NOT NULL,
     population INTEGER     NOT NULL,
     
     PRIMARY KEY (id)
